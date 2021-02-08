cec28dca7149050d4ceb05fbe06df258be46df65
Simran <mdrsimran@gmail.com>
2021-02-08 13:04:42 -0600

    commit tree a030d49a3059c1f5bd5e2b4b3194dab9385479e2

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(n => 
+        n.toLowerCase() === string.toLowerCase()
+    )
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter ( n =>
+        n.charAt(0) === string.charAt(0)
+        )
+}
+
+function matchName(drivers, string){
+    return drivers.filter ( n =>
+        n.name === string)
+}
+
